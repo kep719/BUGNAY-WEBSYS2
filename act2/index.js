4e3b646732115673f8d62799a90538720d4e7226
kep719 <20234719@s.ubaguio.edu>
2025-09-05 18:42:22 +0800

    Update index.js

--- act2/index.js
+++ act2/index.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const app = express();
-const customerRoutes = require('./src/customer/customer-routes');
-const productRoutes = require('./src/product/product-routes');
-const orderRoutes = require('./src/order/order-routes');
+const express = require('express')
+const app = express()
+const customerRoutes = require('./src/customer/customer-routes')
+const productRoutes = require('./src/product/product-routes')
+const orderRoutes = require('./src/order/order-routes')
 
-app.use(express.json());
+app.use(express.json())
 
-app.use('/customer', customerRoutes);
-app.use('/product', productRoutes);
-app.use('/order', orderRoutes);
+app.use('/customer', customerRoutes)
+app.use('/product', productRoutes)
+app.use('/order', orderRoutes)
 
 app.listen(3000, () => {
     console.log('running on http://localhost:3000')
